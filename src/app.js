72bbaa857d40a34bccf8b73b230570aa161b2f3a
Andrés Ibarra Guerrero <andresmcio@Andress-MacBook-Pro.local>
2024-11-29 03:06:38 -0500

    Reversing https

--- src/app.js
+++ src/app.js
@@ -1,19 +1,12 @@
 require('dotenv').config();
-const { readFileSync } = require('fs');
 const { resolve } = require('path');
 const express = require('express');
-const https = require('https');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const i18n = require('./config/i18n.config');
 const languageMiddleware = require('./middlewares/language.middleware');
 const app = express();
 const { port, callback } = require('./modules/port');
-
-const options = {
-    key: readFileSync('./take2films_key.pem'),
-    cert: readFileSync('./take2films_cert.pem')
-}
 
 app.set('views', resolve(__dirname, './views'));
 app.set('view engine', 'ejs');
@@ -31,6 +24,4 @@ app.use('/', require('./routes/main.routes'));
 app.use('/users', require('./routes/users.routes'));
 app.use('*', require('./routes/404.routes'));
 
-https.createServer(options, app).listen(port, () => {
-    callback(port); 
-});
+app.listen(port, callback(port));
